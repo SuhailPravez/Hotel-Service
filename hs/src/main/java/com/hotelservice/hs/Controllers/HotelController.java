78bb3cd710b8350fabf817eba1b1a4817a1cd2f6
Suhail Ahamad <140061255+SuhailPravez@users.noreply.github.com>
2025-07-19 15:27:19 +0530

    Minor Fix

--- hs/src/main/java/com/hotelservice/hs/Controllers/HotelController.java
+++ hs/src/main/java/com/hotelservice/hs/Controllers/HotelController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,7 +37,7 @@ public class HotelController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Hotel> getHotel(@RequestParam String id){
+    public ResponseEntity<Hotel> getHotel(@PathVariable String id){
         Hotel hotel = hotelService.getHotel(id);
        return ResponseEntity.ok(hotel);
     }
